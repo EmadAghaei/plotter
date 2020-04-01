7eaf178ee4cd76c04d63689f60aa9b18f4c51261
Emad Aghayi <emad.aghayi@gmail.com>
2020-03-31 23:07:47 -0400

    add violin plot

---
+++ swarmPlot.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+
+productivity = pd.read_csv('Productivity.csv')
+sns.set(style="whitegrid")
+# productivity = sns.load_dataset("/Users/emadaghayi/Google Drive/PhD Materials Emad/Microtask Programming/Evaluation  Crowd Microservices/Analysis/workspace/Plotter/data/Productivity.xlsx/Productivity.xlsx")
+print(productivity)
+ax = sns.violinplot(x="Developers", y="Number of correctly implemented behaviors", data=productivity, inner=None,  palette=['#ff6361','#58508d']).set_title("Productivity of Individual Developers")
+ax = sns.swarmplot(x="Developers", y="Number of correctly implemented behaviors", data=productivity, color="white", edgecolor="gray")
+plt.savefig('prod.pdf')
