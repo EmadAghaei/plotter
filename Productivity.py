c5aa88c55388547f451620af222ebba737b4dd6d
Emad Aghayi <emad.aghayi@gmail.com>
2020-11-02 17:14:27 -0500

    update prodcutivity.py to show per hour

--- Productivity.py
+++ Productivity.py
@@ -7,11 +7,12 @@ productivity = pd.read_csv('data/Productivity.csv')
 sns.set(style="whitegrid")
 # productivity = sns.load_dataset("/Users/emadaghayi/Google Drive/PhD Materials Emad/Microtask Programming/Evaluation  Crowd Microservices/Analysis/workspace/Plotter/data/Productivity.xlsx/Productivity.xlsx")
 print(productivity)
-ax = sns.violinplot(x="Developers", y="Number of correctly implemented behaviors", data=productivity, inner=None,  palette=['#ff6361','#58508d'])\
+ax = sns.violinplot(x="Developers", y="Number of correctly implemented behaviors per hour", data=productivity, inner=None,  palette=['#ff6361','#58508d'])\
     # .set_title("Productivity of Individual Developers")
-ax = sns.swarmplot(x="Developers", y="Number of correctly implemented behaviors", data=productivity, color="white", edgecolor="gray")
+ax = sns.swarmplot(x="Developers", y="Number of correctly implemented behaviors per hour", data=productivity, color="white", edgecolor="gray")
 # plt.show();
 plt.gca().spines['right'].set_visible(False)
 plt.gca().spines['top'].set_visible(False)
-plt.yticks(np.arange(0, 40, 3))
+plt.yticks(np.arange(0, 8, 1))
+# plt.show();
 plt.savefig('./data/prod.pdf')
